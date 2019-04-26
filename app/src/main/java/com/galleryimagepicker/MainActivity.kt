f9dbf8cf4076ce924cb99c19b62ba37d0e7b0429
DifApparatus <shap1kin2@gmail.com>
2019-04-26 15:07:17 +0300

    Just a button for gallery.

---
+++ app/src/main/java/com/galleryimagepicker/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.galleryimagepicker
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        galleryPickButton.setOnClickListener {
+            pickImage();
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GALLERY_REQUEST && resultCode == Activity.RESULT_OK){
+                val galleryImageUri = data?.data
+                galleryImageView.setImageURI(galleryImageUri)
+        }
+    }
+
+    private fun pickImage(){
+        val intent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+        intent.resolveActivity(packageManager)?.let {
+            startActivityForResult(
+                intent, GALLERY_REQUEST
+            )
+        }
+    }
+    companion object {
+        private const val GALLERY_REQUEST : Int = 1
+        private const val PATH = "image/*"
+    }
+}
